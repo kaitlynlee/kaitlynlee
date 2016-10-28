e20c1866136e4c459c21f5f52f7d0fb9c22cf168
kaitlynlee <kaitlynlee@sbcglobal.net>
2016-10-27 22:57:39 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+ <div class = "footer-cont">
+        <div class = "footer">
+            <div class = "row">
+            <div class = "box one-third">
+                <h3>Resources used:</h3>
+                <a href = "http://supersimpleslider.com/">Image Slider</a><br>
+                <a href = "http://www.graphicsfuel.com/2013/12/social-media-line-icons/">Social Media Icons</a><br>
+                <a href = "http://tympanus.net/codrops/2014/03/21/google-grid-gallery/">Image Gallery</a> 
+            </div>
+            <div class = "box one-third">
+                <h3>Contact</h3>
+                <p>Email: kaitlynlee@sbcglobal.net<br>
+                    Twitter: <a href = "http://twitter.com/jingyuui">jingyuui</a>
+                </p>
+            </div>
+            <div class = "box one-third">
+                <div class = "row">
+                    <h3 style = "float: right">Social Media</h3>
+                </div>
+                <div class = "row">
+                    <div id = "social">
+                        <a href= "http://instagram.com/jingyuui"><img src = "images/social/insta.png"></a><a href= "http://lochbell.deviantart.com"><img src = "images/social/devart.png"></a><a href= "http://souppot.tumblr.com"><img src = "images/social/tumblr.png"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
